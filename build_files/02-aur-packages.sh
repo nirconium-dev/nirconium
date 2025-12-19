80b8d9df65d28be1d8e79c7bd2a6b40ae690e8a5
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-12-19 12:51:43 -0800

    fix user creation

--- build_files/02-aur-packages.sh
+++ build_files/02-aur-packages.sh
@@ -1,16 +1,17 @@
-# setup temp user
-useradd -m -G wheel builder && echo "builder:1234" | chpasswd
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+# setup user
+useradd -m -G wheel builder
+echo "builder:1234" | chpasswd
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/10-installer
 
-# setup yay
+# build yay
 su - builder -c "git clone https://aur.archlinux.org/yay.git ~/yay && \
-    cd ~/yay && \
-    makepkg -si --noconfirm"
+                 cd ~/yay && \
+                 makepkg -si --noconfirm"
 
-# install aur packages
+# install aur pkgs
 su - builder -c "yay -S --noconfirm hypryou hypryou-greeter hypryou-utils"
 
 # cleanup
+rm /etc/sudoers.d/10-installer
 pkill -u builder
 userdel -r builder
-sed -i 's/^%wheel ALL=(ALL:ALL) ALL/# %wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
