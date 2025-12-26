d6dde4befbb14339abf4b54ab9413506feada9b8
certifiedfooliolol <mastersvanity@gmail.com>
2025-12-26 04:13:36 +0000

    feat: custom fastfetch

---
+++ system_files/etc/profile.d/nirconium-fastfetch.sh
@@ -0,0 +1,3 @@
+alias fastfetch="fastfetch --logo /usr/share/nirconium/ascii-logo/logo.txt"
+alias neofetch="fastfetch --logo /usr/share/nirconium/ascii-logo/logo.txt"
+alias hyfetch="fastfetch --logo /usr/share/nirconium/ascii-logo/logo.txt"
